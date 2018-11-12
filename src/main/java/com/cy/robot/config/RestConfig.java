9cadde1fe9ee899fd6f50b57c9d6fbe525dee1e5
DESKTOP-O51HBF8\chenyang <pjchenyang@qq.com>
2018-11-12 22:02:59 +0800

    no message

--- src/main/java/com/cy/robot/config/RestConfig.java
+++ src/main/java/com/cy/robot/config/RestConfig.java
@@ -1,22 +1,12 @@
 package com.cy.robot.config;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.http.converter.FormHttpMessageConverter;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 public class RestConfig {
