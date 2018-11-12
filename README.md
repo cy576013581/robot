42e22b8e182450240e581fd28fa07865c15d1cc7
DESKTOP-O51HBF8\chenyang <pjchenyang@qq.com>
2018-11-12 21:58:34 +0800

    解决天气预报获取乱码的bug，原因是接口数据是经过 GZIP 压缩过的，使用HttpClient 解决，其内置了对于 GZIP 的支持

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # robot
 this is robot
+
+#### 开发日志
+
+- 解决天气预报获取乱码的bug，原因是接口数据是经过 GZIP 压缩过的，使用HttpClient 解决，其内置了对于 GZIP 的支持   2018-11-12
--- src/main/java/com/cy/robot/config/RestConfig.java
+++ src/main/java/com/cy/robot/config/RestConfig.java
@@ -3,8 +3,10 @@ package com.cy.robot.config;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
@@ -14,15 +16,17 @@ import org.springframework.web.client.RestTemplate;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class RestConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        StringHttpMessageConverter m = new StringHttpMessageConverter(Charset.forName("UTF-8"));
-        RestTemplate restTemplate = new RestTemplateBuilder().additionalMessageConverters(m).build();
-
+        RestTemplate restTemplate = new RestTemplate(
+                new HttpComponentsClientHttpRequestFactory()); // 使用HttpClient，支持GZIP
+        restTemplate.getMessageConverters().set(1,
+                new StringHttpMessageConverter(StandardCharsets.UTF_8)); // 支持中文编码
         return restTemplate;
     }
 }
