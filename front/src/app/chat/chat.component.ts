1d7f5f67b2e52ba87672dd98c5fb52f3ce34b053
DESKTOP-O51HBF8\chenyang <pjchenyang@qq.com>
2018-11-13 22:12:50 +0800

    完善聊天界面

--- front/src/app/chat/chat.component.ts
+++ front/src/app/chat/chat.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {NzMessageService} from "ng-zorro-antd";
+import {Text} from "./text";
 
 @Component({
   selector: 'app-chat',
@@ -14,7 +15,7 @@ export class ChatComponent implements OnInit {
 
   text : string;
 
-  imessages : string[] = [];
+  imessages : Text[] = [];
 
   constructor(private message: NzMessageService) { }
 
@@ -26,7 +27,9 @@ export class ChatComponent implements OnInit {
       this.message.error("请输入你要问的问题哟！")
     } else{
       this.isLoading = true;
-      this.imessages.push(this.text);
+
+      this.imessages.push(new Text(2,this.text));
+      this.imessages.push(new Text(1,"您输入的是："+this.text));
       this.text="";
       this.isLoading = false;
     }
---
+++ front/src/app/chat/text.ts
@@ -0,0 +1,13 @@
+import {type} from "os";
+
+export class Text{
+
+  type?: number;
+  text?: string;
+
+  constructor(type?: number,text?: string) {
+    this.type = type;
+    this.text = text;
+
+  }
+}
